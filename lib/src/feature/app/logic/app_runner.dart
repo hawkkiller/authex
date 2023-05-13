23fadc434cb38d7738bae5da0b865400fcb6e9ef
Michael Lazebnyi <62852417+hawkkiller@users.noreply.github.com>
2023-05-13 19:32:25 +0300

    Initial commit

---
+++ lib/src/feature/app/logic/app_runner.dart
@@ -0,0 +1,36 @@
+import 'dart:ui';
+
+import 'package:bloc_concurrency/bloc_concurrency.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sizzle_starter/src/core/bloc/observer.dart';
+import 'package:sizzle_starter/src/core/utils/logger.dart';
+import 'package:sizzle_starter/src/feature/app/widget/app.dart';
+import 'package:sizzle_starter/src/feature/initialization/logic/initialization_processor.dart';
+import 'package:sizzle_starter/src/feature/initialization/logic/initialization_steps.dart';
+import 'package:sizzle_starter/src/feature/initialization/model/initialization_hook.dart';
+
+/// A class which is responsible for initialization and running the app.
+class AppRunner with InitializationSteps, InitializationProcessor, InitializationFactoryImpl {
+  /// run initialization
+  ///
+  /// if success -> run app
+  Future<void> initializeAndRun(InitializationHook hook) async {
+    final bindings = WidgetsFlutterBinding.ensureInitialized()..deferFirstFrame();
+    FlutterError.onError = Logger.logFlutterError;
+    PlatformDispatcher.instance.onError = Logger.logPlatformDispatcherError;
+    Bloc.observer = AppBlocObserver();
+    Bloc.transformer = sequential();
+
+    final result = await processInitialization(
+      steps: initializationSteps,
+      hook: hook,
+      factory: this,
+    );
+    bindings.addPostFrameCallback((_) {
+      bindings.allowFirstFrame();
+    });
+    // Run application
+    App(result: result).run();
+  }
+}
---
+++ packages/rest_client/lib/src/exception/network_exception.dart
@@ -0,0 +1,32 @@
+import 'package:meta/meta.dart';
+
+@immutable
+abstract class NetworkException implements Exception {}
+
+@immutable
+class RestClientException implements NetworkException {
+  const RestClientException({
+    this.message,
+    this.statusCode,
+  });
+
+  final String? message;
+  final int? statusCode;
+
+  @override
+  String toString() => 'RestClientException(message: $message, statusCode: $statusCode)';
+}
+
+@immutable
+class InternalServerException implements NetworkException {
+  const InternalServerException({
+    this.message,
+    this.statusCode,
+  });
+
+  final String? message;
+  final int? statusCode;
+
+  @override
+  String toString() => 'InternalServerErrorException(message: $message, statusCode: $statusCode)';
+}
